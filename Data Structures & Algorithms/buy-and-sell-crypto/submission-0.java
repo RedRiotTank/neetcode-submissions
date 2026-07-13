6cffff496cb2a92d6c750ca9a251b703b0422659
Alberto Plaza <albertoplazamontesdm@gmail.com>
2026-07-13 13:07:10 +0200

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int left = 0;
+
+        for (int right = 1; right < prices.length; right++) {
+
+            if (prices[right] < prices[left]) {
+                left = right; 
+            } else {
+                int profit = prices[right] - prices[left];
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+
+        }
+        
+        return maxProfit;
+    }
+}
